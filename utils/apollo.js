faed19464a58744cd7c97b5b880b8ee54198bb77
Tim Au Yeung <tim.ay@talkboxapp.com>
2021-06-17 18:32:13 +0800

    fix graphql host

--- utils/apollo.js
+++ utils/apollo.js
@@ -2,9 +2,14 @@ import { GraphQLClient } from "graphql-request";
 
 let _client = null;
 
-export const getGraphQLClient = () =>
-  (_client = new GraphQLClient(process.env.GRAPHQL_ENDPOINT, {
+export const getGraphQLClient = () => {
+  const domain =
+    typeof window === "undefined"
+      ? "http://127.0.0.1:3000"
+      : window.location.origin;
+  return new GraphQLClient(`${domain}/api/graphql`, {
     headers: {
       authorization: "Bearer MY_TOKEN",
     },
-  }));
+  });
+};
