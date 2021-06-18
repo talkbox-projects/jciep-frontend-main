5e350aadf717f4abd7c6b83e574ebf05b48f639d
Tim Au Yeung <tim.ay@talkboxapp.com>
2021-06-18 18:46:36 +0800

    build error fix

--- pages/404.js
+++ pages/404.js
@@ -6,13 +6,13 @@ const PAGE_KEY = "404";
 export const getStaticProps = async (context) => {
   return {
     props: {
-      page: await getPage({ key: PAGE_KEY, lang: context.locale }),
-      header: await getConfiguration({ key: "header", lang: context.locale }),
-      footer: await getConfiguration({ key: "footer", lang: context.locale }),
-      navigation: await getConfiguration({
-        key: "navigation",
-        lang: context.locale,
-      }),
+      //   page: await getPage({ key: PAGE_KEY, lang: context.locale }),
+      //   header: await getConfiguration({ key: "header", lang: context.locale }),
+      //   footer: await getConfiguration({ key: "footer", lang: context.locale }),
+      //   navigation: await getConfiguration({
+      //     key: "navigation",
+      //     lang: context.locale,
+      //   }),
     },
   };
 };
