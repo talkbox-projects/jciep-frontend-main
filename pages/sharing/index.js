053340aa9a44596e49155246a824bafabaa7bef0
Bilal Saeed <bilalsaeedhb@gmail.com>
2021-06-23 20:44:28 +0500

    Completed Featured Article view

--- pages/sharing/index.js
+++ pages/sharing/index.js
@@ -34,6 +34,14 @@ const Sharing = ({ page, setting, lang }) => {
   const [latestPosts, setLatestPosts] = React.useState([]);
   const [latestPostsPage, setLatestPostsPage] = React.useState(1);
   const [featuredArticle, setFeaturedArticle] = React.useState({});
+  const getCategoryData = (key) => {
+    const category = categories.find(c => c.key === key);
+    const data = {};
+    data.label = lang === "en" ? category.label.en : category.label.zh;
+    data.icon = category.image;
+    data.bgColor = category.color;
+    return data;
+  }
 
   React.useEffect(() => {
     fetchData();
@@ -76,10 +84,9 @@ const Sharing = ({ page, setting, lang }) => {
       )}
       {/* Featured Article Section */}
       <Box
-        h="calc(50vw - 40px)"
+        minH="calc(50vw - 40px)"
         w="100%"
         position="relative"
-        overflowY="hidden"
         background={page?.content?.bannerColor}
         pt={["0", "0", "159px"]}
         px={["0", "0", "203px"]}
@@ -89,8 +96,19 @@ const Sharing = ({ page, setting, lang }) => {
           <chakra.span backgroundImage="linear-gradient(#fff, #fff)" textAlign="left" ml="43" fontSize={["0", "0", "36px"]} lineHeight={2} backgroundRepeat="no-repeat" backgroundPosition="0 0.5em" zIndex="2" pos="relative" px="15px" pb="6px">{page?.content?.title}</chakra.span>
         </chakra.span>
         {featuredArticle && (
-          <Box mt={["0", "0", "24px"]}>
-            <Image h="330px" w="429px" background={featuredArticle.coverImage} mr="34px" />
+          <Box mt={["0", "0", "24px"]} display="flex">
+            <Image h="330px" w="429px" src={featuredArticle.coverImage} mr="34px" zIndex={1} />
+            <Box display="flex" flexDir="column">
+              <Image src={getCategoryData(featuredArticle.category).icon} mb="16px" w="48px" h="40px" />
+              <Box mb="8px">
+                <Box fontSize="12px" background={getCategoryData(featuredArticle.category).bgColor} borderRadius="19px" px="9px" mr="9px" display="inline">
+                  {getCategoryData(featuredArticle.category).label}
+                </Box>
+                <Text fontSize="12px">{featuredArticle.publishDate}</Text>
+              </Box>
+              <Text fontSize="48px" fontWeight="bold" mb="8px">{featuredArticle.title}</Text>
+              <Text fontSize="16px" mb="49px">{featuredArticle.excerpt}</Text>
+            </Box>
           </Box>
         )}
         <Image position="absolute" left="0" bottom="0" src={page?.content?.bottomBorderFeatured} width="100%" fit="contain" />
