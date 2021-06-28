ddeed08e6eda7bf8f18dce00ec011208ccba035a
Tim Au Yeung <tim.ay@talkboxapp.com>
2021-06-28 13:05:40 +0800

    fix post not update

--- pages/_app.js
+++ pages/_app.js
@@ -70,6 +70,6 @@ const App = ({ Component, pageProps }) => {
 export default withTina(App, {
   media: new MongooseMediaStore(),
   plugins: [MarkdownFieldPlugin],
-  enabled: true,
+  enabled: false,
   sidebar: true,
 });
