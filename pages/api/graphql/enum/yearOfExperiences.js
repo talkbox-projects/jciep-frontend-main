9e431971ba29b07ad34efaf8a9f77bcf7860b589
Soap Wong <soap.wong@talkboxapp.com>
2021-07-16 21:01:23 +0800

    update enums

--- pages/api/graphql/enum/yearOfExperiences.js
+++ pages/api/graphql/enum/yearOfExperiences.js
@@ -1,4 +1,5 @@
 export default {
+  unselected: { en: "-", zh: "-" },
   years_0_2: { en: "0-2 years", zh: "0-2年" },
   years_3_5: { en: "3-5 years", zh: "3-5年" },
   years_6_8: { en: "6-8 years", zh: "6-8年" },
