6bf3a30e736629171793da038ab8093608e34ee3
Soap Wong <soap.wong@talkboxapp.com>
2021-07-19 17:58:04 +0800

    added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "react/prop-types": 0,
+    "react/jsx-uses-react": "warning",
+    "react/jsx-uses-vars": "warning",
+  },
+};
