eabdd192a34e8c8bb206b1ec4b311113ee3b2ffa
Rajatgouri020 <84343381+Rajatgouri020@users.noreply.github.com>
2021-07-21 12:57:48 +0530

    added link of youtube

--- pages/resources/index.js
+++ pages/resources/index.js
@@ -96,7 +96,7 @@ const Resources = ({ page }) => {
             px={2}
             mb={36}
             fontWeight="bold"
-            fontSize={["3xl", "5xl", "6xl"]}
+            fontSize={["3xl", "4xl", "5xl"]}
             bg="#F6D644"
           >
             {page?.content?.heroBannerSection["title 標題"]}
