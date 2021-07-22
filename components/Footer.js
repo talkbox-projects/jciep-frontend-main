2a08ad9b9181dcd8d143c3757a23dd8cdfaf7b7e
Tim Au Yeung <tim.ay@talkboxapp.com>
2021-07-22 13:54:45 +0800

    margin logo removed

--- components/Footer.js
+++ components/Footer.js
@@ -36,11 +36,11 @@ const Footer = ({ footer }) => {
                     {(partners ?? []).map(
                       ({ id: _id, label, url, logo = "" }) => {
                         return (
-                          <WrapItem key={_id} maxH={12}>
+                          <WrapItem spacing={0} pr={2} key={_id} maxH={1162}>
                             <Link href={url} key={_id}>
                               <Image
                                 alt={label}
-                                maxH={12}
+                                maxH={16}
                                 h="100%"
                                 src={logo}
                               />
@@ -72,44 +72,52 @@ const Footer = ({ footer }) => {
               columns={[2, 2, 3, 5, 5]}
               gap={12}
             >
-              {(footer?.sitemap ?? []).map(({ id, links = [], title, url, social= [] }) => (
-                <VStack key={id} align="start">
-                  <NextLink href={url}>
-                    <Button
-                      textAlign="left"
-                      variant="link"
-                      color="black"
-                      fontWeight="bold"
-                      fontSize={["xl", "xl", "lg"]}
-                    >
-                      {title}
-                    </Button>
-                  </NextLink>
-                  <Text w="100%">
-                  {
-                    (social ?? []).map(({icon, id, url})=>{
-                      return <a href={url}><Image display="inline-flex" src={icon} height="30px"></Image></a>
-                    })
-                  }
-                  </Text>
-                  
-                  {(links ?? []).map(({ id: _id, url = "/", label = "" }) => {
-                    return (
-                      <NextLink key={_id} id={_id} href={url}>
-                        <Button
-                          fontSize={["xl", "xl", "lg"]}
-                          textAlign="left"
-                          variant="link"
-                          fontWeight="normal"
-                          color="black"
-                        >
-                          {label}
-                        </Button>
-                      </NextLink>
-                    );
-                  })}
-                </VStack>
-              ))}
+              {(footer?.sitemap ?? []).map(
+                ({ id, links = [], title, url, social = [] }) => (
+                  <VStack key={id} align="start">
+                    <NextLink href={url}>
+                      <Button
+                        textAlign="left"
+                        variant="link"
+                        color="black"
+                        fontWeight="bold"
+                        fontSize={["xl", "xl", "lg"]}
+                      >
+                        {title}
+                      </Button>
+                    </NextLink>
+                    <Text w="100%">
+                      {(social ?? []).map(({ icon, id, url }) => {
+                        return (
+                          <a href={url}>
+                            <Image
+                              display="inline-flex"
+                              src={icon}
+                              height="30px"
+                            ></Image>
+                          </a>
+                        );
+                      })}
+                    </Text>
+
+                    {(links ?? []).map(({ id: _id, url = "/", label = "" }) => {
+                      return (
+                        <NextLink key={_id} id={_id} href={url}>
+                          <Button
+                            fontSize={["xl", "xl", "lg"]}
+                            textAlign="left"
+                            variant="link"
+                            fontWeight="normal"
+                            color="black"
+                          >
+                            {label}
+                          </Button>
+                        </NextLink>
+                      );
+                    })}
+                  </VStack>
+                )
+              )}
             </SimpleGrid>
           </Stack>
 
